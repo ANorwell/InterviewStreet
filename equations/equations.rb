130bb7d5b4fa1b93aa31a7f09e394aed5403b616
Arron Norwell <anorwell@gmail.com>
2012-03-21 18:52:41 -0400

    more efficient, accepted version

--- equations/equations.rb
+++ equations/equations.rb
@@ -15,17 +15,84 @@ class Prime
   end
 end
 
+#Returns a list of primes less than n
 def primes(n)
-  primes = (2..n).select do |x|
-    (2..Math.sqrt(x)).none?{|d| x.modulo(d) == 0 }
+  root = n**0.5
+  nums = (0..n).to_a
+  nums[1] = 0
+  for i in (2..root+1)
+    if nums[i] != 0
+      for j in (i*i..n).step(i)
+        nums[j] = 0
+      end
+    end
   end
-  primes
+  nums.select{|x| x != 0 }
 end
 
-def divisors(num, prime_list, hash)
+#Finds one factor of num, and saves the factor and dividend in memo.
+def divisors(num, prime_list, memo)
+  if memo[num]
+    return memo[num]
+  end
+  for p in prime_list
+    if num.modulo(p) == 0
+      memo[num] = [p, num/p]
+      break
+    end
+  end
+end
+
+def time_block(name)
+  t0 = Time.now
+  yield
+  t1 = Time.now
+  #puts "Step #{name} took #{t1-t0} s"
+end
+
+def count_2x_factors(n)
+  #For each number in 2..n, find a single factor
+  primes = []
+  time_block("primes") { primes = primes(n) }
+  memo = Array.new(n+1)
+  primes.each{|p| memo[p] = [p,1]}
+  time_block("divisors") do
+    for i in (2..n)
+      divisors(i,primes,memo)
+    end
+  end
+
+  #Compute the prime factorization of n!**2, with counts in a hash
+  counts = {}
+  primes.each{|p| counts[p] = 0 }
+  time_block("getting counts") do
+    for i in (2..n)
+      val = i
+      begin
+        p, val = memo[val]
+        counts[p] += 1
+      end while val > 1
+    end
+  end
+
+  #Count all the factors
+  factors = 1
+  time_block("adding factors") do 
+    primes.each do |p|
+      factors *= counts[p]*2+1
+      factors = factors.modulo(MOD)
+    end
+  end
+  factors
+end
+
+val = STDIN.gets.to_i
+puts count_2x_factors(val)
+
+#calculates the divisors of num by storing their counts in the provided hash
+def divisors_old(num, prime_list, hash)
   sqrt = Math.sqrt(num)
   for p in prime_list
-
     if num == 1
       break
     end
@@ -47,7 +114,7 @@ def count_doubled_factors(n)
   hash = {}
   primes.each{|p| hash[p] = 0 }
   for i in (2..n)
-    divisors(i,primes, hash)
+    divisors_old(i,primes, hash)
   end
   factors = 1
   primes.each do |p|
@@ -56,5 +123,3 @@ def count_doubled_factors(n)
   end
   factors
 end
-
-puts count_doubled_factors(STDIN.gets.to_i)
