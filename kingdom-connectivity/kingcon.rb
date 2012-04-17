6f963dc1e4ebe43d1238b592970010795b5ee527
Arron Norwell <anorwell@gmail.com>
2012-04-17 18:14:11 -0400

    cleanup

--- kingdom-connectivity/kingcon.rb
+++ kingdom-connectivity/kingcon.rb
@@ -1,5 +1,6 @@
-#each node represents a city
+#A Node in a di-graph; maintains a list of children and a list of parents
 class Node
+  #Node states, as set by a DFS.
   UNVISITED = 0          #a completely untouched node
   CAN_REACH_TARGET = 1   #a node from which there is a path to the target node
   VISITED = 2            #a node that has been visited in the forward DFS
@@ -11,14 +12,8 @@ class Node
     @paths = 0
   end
 
-  def paths(arg=nil)
-    if arg
-      @paths = arg
-    else
-      @paths
-    end
-  end
-
+  attr_accessor :children, :parents, :visit, :id, :paths, :position
+  
   def add_child(node)
     if @children[node]
       @children[node] += 1
@@ -33,33 +28,17 @@ class Node
     end
   end
 
-  def children
-    @children
-  end
-
-  def parents
-    @parents
-  end
-
-  def visit
-    @visit
-  end
-
-  def id
-    @id
-  end
-
   def display
-    puts "#{@id} : #{@children.map{|k,v| "#{k.id}=>#{v}" }.to_s} : #{@visit} : (Parents: #{@parents.map{|k,v| "#{k.id}=>#{v}" }.to_s} )"
+    puts "#{@id} : Children: #{@children.to_s} : Visit status: #{@visit} : Parents: #{@parents.to_s}"
   end
 
-  #Does a reverse DFS of the graph, to check recursively which nodes can reach
+  #Do a reverse DFS of the graph, to check recursively which nodes can reach
   #the current node.
-  def prune
+  def find_reaching_nodes
     @visit = CAN_REACH_TARGET
     for node, count in @parents
       if node.visit == UNVISITED
-        node.prune
+        node.find_reaching_nodes
       end
     end
   end
@@ -81,39 +60,40 @@ class Node
   end
 end
 
-class KingdomConnector
+class PathCounter
   MOD = 10**9 #the number of paths is mod this number.
   def initialize
-    @n,@m = STDIN.readline.split.map{|x| x.to_i }
+    @n,@m = STDIN.readline.split.map{ |x| x.to_i }
     @nodes = Array.new(@n.to_i+1)
     for i in 1..@n.to_i
       @nodes[i] = Node.new(i)
     end
     for i in 1..@m.to_i
-      source,dest = STDIN.readline.split.map{|x| x.to_i }
+      source,dest = STDIN.readline.split.map{ |x| x.to_i }
       @nodes[source].add_child(@nodes[dest])
     end
   end
 
   def display
-    @nodes.each{|n| n.display }
+    @nodes.each{ |n| n.display }
   end
 
   #returns the number of paths from @nodes[1] to @nodes[@n]
+  #see README for description of algorithm used here
   def count_paths
-    @nodes[@n].prune
+    @nodes[@n].find_reaching_nodes
     if not @nodes[1].visit == Node::CAN_REACH_TARGET
       return 0
     end
     sorted_nodes = @nodes[1].top_sort
     for i in 0...sorted_nodes.length
-      sorted_nodes[i].position(i)
+      sorted_nodes[i].position = i
       if sorted_nodes[i].id == 1
         start_pos = i
       end
     end
     init = sorted_nodes[start_pos]
-    init.paths(1)
+    init.paths = 1
     for i in start_pos...sorted_nodes.length
       cur = sorted_nodes[i]
       for child,count in cur.children
@@ -129,11 +109,11 @@ class KingdomConnector
       for prev,count in cur.parents
         paths += prev.paths*count
       end
-      cur.paths(paths.modulo(MOD))
+      cur.paths = paths.modulo(MOD)
     end
     @nodes[@n].paths.modulo(MOD)
   end
 end
 
-kc = KingdomConnector.new
-puts kc.count_paths
+pc = PathCounter.new
+puts pc.count_paths
